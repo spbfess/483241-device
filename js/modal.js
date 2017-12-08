8aaf1a9c0951fb68394b1946802d8acd0f188d7e
Dmitry Nefyodov <dmitry.nefyodov@nordigy.ru>
2017-12-08 18:03:08 +0300

    applied autoprefixer, minifier for css/js; added transparencey for modal closebackground; removed imgs not used

--- js/modal.js
+++ js/modal.js
@@ -27,19 +27,19 @@ var mapPopup = document.querySelector(".modal-map");
 var mapClose = mapPopup.querySelector(".modal-close");
 
 writeUs.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	writeUsPopup.classList.add("modal-show");
-	if (writeUsLoginCached) {
-		writeUsLogin.value = writeUsLoginCached;
-		if (writeUsEmailCached) {
-			writeUsEmail.value = writeUsEmailCached;
-			writeUsMessage.focus();
-		} else {
-		writeUsEmail.focus();		      	
-		}
-	} else {
-		writeUsLogin.focus();
-	}
+        evt.preventDefault();
+        writeUsPopup.classList.add("modal-show");
+        if (writeUsLoginCached) {
+                writeUsLogin.value = writeUsLoginCached;
+                if (writeUsEmailCached) {
+                        writeUsEmail.value = writeUsEmailCached;
+                        writeUsMessage.focus();
+                } else {
+                writeUsEmail.focus();
+                }
+        } else {
+                writeUsLogin.focus();
+        }
 });
 
 writeUsClose.addEventListener("click", function (evt) {
